7d7f087d0b21f21678063989170bb01ab32a335b
Purnendu.Kar <purnendu.kar8@gmail.com>
2026-03-27 15:15:44 +0530

    spring initial setup

---
+++ src/main/java/com/example/scaler_capstone_project/ScalerCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.scaler_capstone_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerCapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerCapstoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scaler_capstone_project/ScalerCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scaler_capstone_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
