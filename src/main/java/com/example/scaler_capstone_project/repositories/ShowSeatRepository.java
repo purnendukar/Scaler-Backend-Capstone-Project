122a5fcdf3846da2006790b27d50dfda8ffbc3c1
Purnendu Kar <purnendu.kar8@gmail.com>
2026-03-27 15:59:26 +0530

    feat: ShowSeatRepository

---
+++ src/main/java/com/example/scaler_capstone_project/repositories/ShowSeatRepository.java
@@ -0,0 +1,13 @@
+package com.example.scaler_capstone_project.repositories;
+
+import com.example.scaler_capstone_project.models.ShowSeat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShowSeatRepository extends JpaRepository<ShowSeat, Long> {
+    @Override
+    List<ShowSeat> findAllById(Iterable<Long> ids);
+}
