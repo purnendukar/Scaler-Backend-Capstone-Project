2bc08e2ddc8a868600e197d9f9f89868faa2543d
Purnendu Kar <purnendu.kar8@gmail.com>
2026-03-31 13:11:59 +0530

    feat: Exceptions

---
+++ src/main/java/com/example/scaler_capstone_project/exceptions/SeatsNoLongerAvailableException.java
@@ -0,0 +1,7 @@
+package com.example.scaler_capstone_project.exceptions;
+
+public class SeatsNoLongerAvailableException extends RuntimeException{
+    public SeatsNoLongerAvailableException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/scaler_capstone_project/exceptions/ShowNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.scaler_capstone_project.exceptions;
+
+public class ShowNotFoundException extends RuntimeException{
+    public ShowNotFoundException(String message) {
+        super(message);
+    }
+}
