b0e3ca31c9fe257d9b93498352a9649aec3f79b2
Purnendu Kar <purnendu.kar8@gmail.com>
2026-04-02 15:48:44 +0530

    feat: login api

--- src/main/java/com/example/scaler_capstone_project/controllers/UserController.java
+++ src/main/java/com/example/scaler_capstone_project/controllers/UserController.java
@@ -8,15 +8,21 @@ import com.example.scaler_capstone_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.scaler_capstone_project.dtos.LoginRequestDTO;
+import com.example.scaler_capstone_project.dtos.LoginResponseDTO;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class UserController {
     @Autowired
     private UserService userService;
 
     //localhost:8080/user/signup
-    @PostMapping("/user/signup")
+    @PostMapping("/signup")
     public SignUpResponseDTO signup(@RequestBody SignUpRequestDTO signUpRequestDTO){
         SignUpResponseDTO signupResponseDTO = new SignUpResponseDTO();
         try{
@@ -31,5 +37,25 @@ public class UserController {
         }
 
         return signupResponseDTO;
+    }
+    
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO){
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
+        try{
+            Map<String, Object> loginResult = userService.login(loginRequestDTO.getEmail(),
+                    loginRequestDTO.getPassword());
+
+            User user = (User) loginResult.get("user");
+            String token = (String) loginResult.get("token");
+
+            loginResponseDTO.setUser(user);
+            loginResponseDTO.setResponseStatus(ResponseStatus.SUCCESS);
+            loginResponseDTO.setToken(token);
+        }catch (Exception ex){
+            loginResponseDTO.setResponseStatus(ResponseStatus.FAILURE);
+        }
+
+        return loginResponseDTO;
     }
 }
